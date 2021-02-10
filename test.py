3a3a4b7f968a71ac205397a340f6c3104bcad1e5
Marco <viaimperiale@gmail.com>
2021-02-10 16:35:00 +0100

    File di test per testare sunny-cp python2 vs python3 sui file test/examples/*

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
-from subprocess import run
 import os
+from subprocess import run
+
 sunny2 = '/home/marco/Documents/uni/tesi/sunny-cp2/bin/sunny-cp'
 sunny = '/home/marco/Documents/uni/tesi/sunny-cp/bin/sunny-cp'
 mzns = [f'/home/marco/Documents/uni/tesi/sunny-cp/test/examples/{file}' for file in os.listdir("test/examples")]
+
 skip = ["/home/marco/Documents/uni/tesi/sunny-cp/test/examples/battleships_1.mzn",
         "/home/marco/Documents/uni/tesi/sunny-cp/test/examples/alpha.mzn"]
 for test in mzns:
@@ -12,8 +14,10 @@ for test in mzns:
     sun2 = run(["python2", sunny2, test], capture_output=True)
     print(test)
     for s1, s2 in zip(sorted(sun1.stdout.decode("utf8").split("\n")), sorted(sun2.stdout.decode("utf8").split("\n"))):
-        if s1 != s2 and not (("% Search completed at time: " in s1 and "% Search completed at time: " in s2) or
-        ("% Search completed at time: " in s1 and "% Search completed at time: " in s2) or
-        ("% Current Solution Time: " in s1 and "% Current Solution Time: " in s2)):
+        if s1 != s2 and not (
+                ("% Search completed at time: " in s1 and "% Search completed at time: " in s2) or
+                ("% Search completed at time: " in s1 and "% Search completed at time: " in s2) or
+                ("% Current Solution Time: " in s1 and "% Current Solution Time: " in s2)
+        ):
             print("\t", s1)
             print("\t", s2)
